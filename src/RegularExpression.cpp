1e2b966896ac17308eb4f6b782ba307762c86734
ahmedmagdy711 <ahmedmagdy711@gmail.com>
2016-04-15 22:27:09 +0200

    Implement convertLine function.

--- src/RegularExpression.cpp
+++ src/RegularExpression.cpp
@@ -9,7 +9,24 @@ RegularExpression::RegularExpression(const string lexical_file_name) {
 }
 
 void RegularExpression::convertLine(string line) {
-
+    string builder = "";
+    for(int i = 0 ; i < line.length() ; ++i) {
+        builder.append(&line[i]);
+        if(line[i] == '=' || line[i] == ':') {
+            string key = builder.substr(0, builder.length() - 2);
+            string rest_of_line = line.substr(i + 1, line.length() - 1);
+            string value = evaluate(rest_of_line);
+            if(line[i] == '=')
+                RegularExpression::regular_definetions[key] = value;
+            else
+                RegularExpression::regular_expressions[key] = value;
+        }else if(line[i] == '{') {
+            if(line[i] == ' ') {
+                RegularExpression::keywords.insert(builder);
+                builder = "";
+            }
+        }
+    }
 }
 
 string RegularExpression::evaluate(string line) {
