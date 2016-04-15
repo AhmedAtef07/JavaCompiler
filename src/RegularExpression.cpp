88af6ea064f17c1eb6d00b2f6bbe7701dec12c66
Ramy Tamer <ramytamer94@gmail.com>
2016-04-15 22:23:22 +0200

    Adding lexical input, and RegularExpression class attributes.

--- src/RegularExpression.cpp
+++ src/RegularExpression.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "RegularExpression.h"
+
+RegularExpression::RegularExpression(const string lexical_file_name) {
+
+}
+
+void RegularExpression::convertLine(string line) {
+
+}
+
+string RegularExpression::evaluate(string line) {
+    return std::basic_string<char, char_traits<char>, allocator<char>>();
+}
+
+string RegularExpression::range_closure(char char1, char char2) {
+    return std::basic_string<char, char_traits<char>, allocator<char>>();
+}
+
+
+
+
+
+
+
--- src/RegularExpression.h
+++ src/RegularExpression.h
@@ -6,7 +6,29 @@
 #define JAVACOMPILER_REGULAREXPRESSION_H
 
 
+#include <fstream>
+#include <string>
+#include <set>
+#include <map>
+
+using namespace std;
+
 class RegularExpression {
+
+public:
+
+    set<string> keywords;
+    set<string> punctuations;
+    map<string, string> regular_definetions;
+    map<string, string> regular_expressions;
+
+    RegularExpression(const string lexical_file_name);
+
+private:
+
+    void convertLine(string line);
+    string evaluate(string line);
+    string range_closure(char char1, char char2);
 
 };
 
